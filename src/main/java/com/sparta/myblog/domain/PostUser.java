8f7ed3dabc96b17b047576d4787f07648a838df9
sunho <yalla12@naver.com>
2022-08-05 20:33:19 +0900

    엔티티 설정

--- src/main/java/com/sparta/myblog/domain/PostUser.java
+++ src/main/java/com/sparta/myblog/domain/PostUser.java
@@ -1,4 +1,33 @@
 package com.sparta.myblog.domain;
 
-public class PostUser {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Users")
+public class PostUser extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+
+
 }
